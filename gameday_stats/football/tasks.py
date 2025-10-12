7adf95eba3b23ccc6c23b6469735684cd3078ba0
Seth Nkwo <sethnkwocool@gmail.com>
2025-10-12 11:48:39 +0100

    Added celery to update football league standings hourly

---
+++ gameday_stats/football/tasks.py
@@ -0,0 +1,9 @@
+from celery import shared_task
+from django.core.management import call_command
+
+@shared_task
+def update_standings_task():
+    """
+    Calls update standings command
+    """
+    call_command("update_standings")
--- gameday_stats/gameday_stats/settings.py
+++ gameday_stats/gameday_stats/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from celery.schedules import crontab
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +124,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Periodic schedule for CELERY
+CELERY_BEAT_SCHEDULE = {
+    'update-standings-every-hour': {
+        'task' : 'football.task.update_standings_task',
+        'schedule' : crontab(minute=0, hour='*')
+    }
+}
