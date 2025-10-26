261614d79cd60e8b098510d7e6cf6866e0405aa5
Seth Nkwo <sethnkwocool@gmail.com>
2025-10-26 20:28:49 +0100

    updated to use render postgres

--- gameday_stats/settings.py
+++ gameday_stats/settings.py
@@ -83,23 +83,20 @@ WSGI_APPLICATION = 'gameday_stats.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv('DB_NAME'),
-            "USER": os.getenv('DB_USER'),
-            "PASSWORD": os.getenv('DB_PASSWORD'),
-            "HOST": os.getenv('DB_HOST'),
-            "PORT": os.getenv('DB_PORT'),
-        }
-    }
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": os.getenv('DB_NAME'),
+#             "USER": os.getenv('DB_USER'),
+#             "PASSWORD": os.getenv('DB_PASSWORD'),
+#             "HOST": os.getenv('DB_HOST'),
+#             "PORT": os.getenv('DB_PORT'),
+#         }
 #     }
-# }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 
 # Password validation
